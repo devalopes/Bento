6d1af87b88a1e2d96c9000187e861fc87d9bd96f
Alex Lopes <alexjonathanlopes@gmail.com>
2022-08-06 15:19:22 -0400

    Fix config

--- config.js
+++ config.js
@@ -6,116 +6,181 @@
 // └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
 
 const CONFIG = {
-    // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
-    // ├┴┐├─┤└─┐││  └─┐
-    // └─┘┴ ┴└─┘┴└─┘└─┘
-  
-    // General
-    name: '',
-    imageBackground: false,
-    openInNewTab: true,
-    twelveHourFormat: true,
-  
-    // Greetings
-    greetingMorning: 'Good morning!',
-    greetingAfternoon: 'Good afternoon',
-    greetingEvening: 'Good evening',
-    greetingNight: 'Go to sleep!',
-  
-    // Weather
-    weatherKey: '8f39fad6fa289adb07fd7eecf61e66f1',
-    weatherIcons: 'Nord', // 'Nord', 'Dark', 'White'
-    weatherUnit: 'F', // 'F', 'C'
-    language: 'en', // More languages in https://openweathermap.org/current#multi
-  
-    trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
-    defaultLatitude: '40.320',
-    defaultLongitude: '-74.594',
-  
-    // Links
-    cards: [
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: "",
+  imageBackground: false,
+  openInNewTab: true,
+  twelveHourFormat: true,
+
+  // Greetings
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon",
+  greetingEvening: "Good evening",
+  greetingNight: "Go to Sleep!",
+
+  // Layout
+  bentoLayout: "bento", // 'bento', 'lists', 'buttons'
+
+  // Weather
+  weatherKey: "8f39fad6fa289adb07fd7eecf61e66f1", // Write here your API Key
+  weatherIcons: "Nord", // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: "F", // 'F', 'C'
+  language: "en", // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: "40.320",
+  defaultLongitude: "-74.594",
+
+  // Autochange
+  autoChangeTheme: true,
+
+  // Autochange by OS
+  changeThemeByOS: true,
+
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: false,
+  hourDarkThemeActive: "18:30",
+  hourDarkThemeInactive: "07:00",
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/",
+    },
+    {
+      id: "2",
+      name: "Youtube",
+      icon: "youtube",
+      link: "https://youtube.com",
+    },
+    {
+      id: "3",
+      name: "Spotify",
+      icon: "music",
+      link: "https://spotify.com/",
+    },
+    {
+      id: "4",
+      name: "Mail",
+      icon: "mail",
+      link: "https://gmail.com/",
+    },
+    {
+      id: "5",
+      name: "Twitch",
+      icon: "twitch",
+      link: "https://twitch.tv/",
+    },
+    {
+      id: "6",
+      name: "Amazon",
+      icon: "shopping-cart",
+      link: "https://amazon.com",
+    },
+  ],
+
+  secondButtonsContainer: [
+    // {
+    // 	id: '1',
+    // 	name: 'Music',
+    // 	icon: 'headphones',
+    // 	link: 'https://open.spotify.com',
+    // },
+    // {
+    // 	id: '2',
+    // 	name: 'twitter',
+    // 	icon: 'twitter',
+    // 	link: 'https://twitter.com/',
+    // },
+    // {
+    // 	id: '3',
+    // 	name: 'bot',
+    // 	icon: 'bot',
+    // 	link: 'https://discord.com/app',
+    // },
+    // {
+    // 	id: '4',
+    // 	name: 'Amazon',
+    // 	icon: 'shopping-bag',
+    // 	link: 'https://amazon.com/',
+    // },
+    // {
+    // 	id: '5',
+    // 	name: 'Hashnode',
+    // 	icon: 'pen-tool',
+    // 	link: 'https://hashnode.com/',
+    // },
+    // {
+    // 	id: '6',
+    // 	name: 'Figma',
+    // 	icon: 'figma',
+    // 	link: 'https://figma.com/',
+    // },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
+  firstlistsContainer: [
+    {
+      icon: "coffee",
+      id: "1",
+      links: [
         {
-            id: '1',
-            name: 'Github',
-            icon: 'github',
-            link: 'https://github.com/',
-          },
+          name: "HackerNews",
+          link: "https://news.ycombinator.com",
+        },
         {
-            id: '2',
-            name: 'Youtube',
-            icon: 'youtube',
-            link: 'https://youtube.com',
-          },
+          name: "Hackaday",
+          link: "https://hackaday.io/myFeed",
+        },
         {
-            id: '3',
-            name: 'Spotify',
-            icon: 'music',
-            link: 'https://spotify.com/',
-          },
+          name: "ArsTechnica",
+          link: "https://arstechnica.com",
+        },
         {
-            id: '4',
-            name: 'Mail',
-            icon: 'mail',
-            link: 'https://gmail.com/',
-          },
-        {
-            id: '5',
-            name: 'Twitch',
-            icon: 'twitch',
-            link: 'https://twitch.tv/',
-          },
-        {
-            id: '6',
-            name: 'Amazon',
-            icon: 'shopping-cart',
-            link: 'https://amazon.com',
-          },
+          name: "IEEE",
+          link: "https://spectrum.ieee.org/",
+        },
       ],
-  
-    // Autochange by OS
-    changeThemeByOS: true,
-  
-    //Icons
-    firstListIcon: 'coffee',
-    secondListIcon: 'sidebar',
-  
-    // Links
-    lists: {
-        firstList: [
-            {
-                name: 'HackerNews',
-                link: 'https://news.ycombinator.com',
-              },
-            {
-                name: 'Hackaday',
-                link: 'https://hackaday.io/myFeed',
-              },
-          {
-                name: 'ArsTechnica',
-                link: 'https://arstechnica.com',
-              },
-          {
-                name: 'IEEE',
-                link: 'https://spectrum.ieee.org/',
-              },
-        ],
-        secondList: [
-            {
-                name: 'Neovim',
-                link: 'https://reddit.com/r/neovim',
-              },
-            {
-                name: 'Wondrium',
-                link: 'https://www.wondrium.com/home',
-              },
-          {
-                name: 'Netflix',
-                link: 'https://netflix.com',
-              },
-          {
-                name: 'Prime',
-                link: 'https://primevideo.com',
-              },
-        ],
-      },
-  };
+    },
+  ],
+
+  // Second Links Container
+  secondListsContainer: [
+    {
+      icon: "sidebar",
+      id: "1",
+      links: [
+        {
+          name: "Neovim",
+          link: "https://reddit.com/r/neovim",
+        },
+        {
+          name: "Wondrium",
+          link: "https://www.wondrium.com/home",
+        },
+        {
+          name: "Netflix",
+          link: "https://netflix.com",
+        },
+        {
+          name: "Prime",
+          link: "https://primevideo.com",
+        },
+      ],
+    },
+  ],
+};
